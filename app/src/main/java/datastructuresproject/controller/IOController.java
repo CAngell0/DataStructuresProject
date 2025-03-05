bac368b0886e5566fb5d6c62783400c5a6635a4e
CAngell0 <carsonjesseangell.learn@gmail.com>
2025-03-05 09:43:37 -0700

    Added read and write text metods to IOController

--- app/src/main/java/datastructuresproject/controller/IOController.java
+++ app/src/main/java/datastructuresproject/controller/IOController.java
@@ -7,5 +7,30 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class IOController {
-   
+   public static String readTextFromFile(Controller app, String path){
+      String content = "";
+
+      try (Scanner fileScanner = new Scanner(new File(path))){
+         while(fileScanner.hasNext()){
+            content += fileScanner.nextLine() + "\n";
+         }
+      } catch (NullPointerException | FileNotFoundException error){
+         app.handleError(error);
+      }
+
+      return content;
+   }
+
+   public static void writeTextToFile(Controller app, String text, String path){
+      try (
+         PrintWriter textWriter = new PrintWriter(new File(path));
+         Scanner stringScanner = new Scanner(text)
+      ){
+         while (stringScanner.hasNextLine()){
+            textWriter.println(stringScanner.nextLine());
+         }
+      } catch (FileNotFoundException error){
+         app.handleError(error);
+      }
+   }
 }
